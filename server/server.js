cf9657f143f9b52b1e0b3586aa13a19508db5eed
Marcus <marcus.ats94@gmail.com>
2025-03-29 14:15:29 +0000

    Created / endpoint, added pg and cors

--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import pg from "pg";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
+const { Pool } = pg;
 
-app.get("/", (req, res) => {
-  res.json({ success: "/ endpoint working" });
+const pool = new Pool({
+  connectionString: process.env.DB_STRING,
+});
+
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT * FROM expenses;");
+  res.json(result.rows);
 });
 
 app.listen(PORT, () => {
